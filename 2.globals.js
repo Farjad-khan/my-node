9b42912e0b87db581f6ee917aca4923065dac2e1
Farjad Ahmed Khan <71921754+Farjad-khan@users.noreply.github.com>
2021-04-19 15:31:58 +0500

    first commit

---
+++ 2.globals.js
@@ -0,0 +1,12 @@
+// Globals  - No Window
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function ton use modules (CommonJs)
+// module     - info about current module(file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("farjad ahmed khan");
+}, 1000);
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = "SUPER SECRET";
+// share
+const Chand = "Chand";
+const Saad = "Saad";
+
+module.exports = { Chand, Saad };
